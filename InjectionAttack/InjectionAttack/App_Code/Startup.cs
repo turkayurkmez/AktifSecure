8cfaf6a55561dcad74836554b0b4fd71852396ac
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-11-24 11:12:33 +0300

    initialize git repo

---
+++ InjectionAttack/InjectionAttack/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InjectionAttack.Startup))]
+namespace InjectionAttack
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
